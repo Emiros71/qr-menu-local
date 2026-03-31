3b035dcc77763f2e669e932aaa3e038b3cf231b5
Emir Altınsay <emir.altinsay@cpank.com>
2026-03-31 15:28:11 +0300

    Remove Vercel analytics from on-prem deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,7 +18,6 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
-        <Analytics />
       </body>
     </html>
   );
